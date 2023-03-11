8f891b58d5f431f7670d046018a2b1f990b28e44
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2023-03-11 08:48:19 -0300

    S5TestNGFrameworkForSelenium

---
+++ src/main/java/S5TestNGFrameworkForSelenium/E38ParallelTestingInvocationCount/InvocationCountDemo.java
@@ -0,0 +1,12 @@
+package S5TestNGFrameworkForSelenium.E38ParallelTestingInvocationCount;
+
+import org.testng.annotations.Test;
+
+public class InvocationCountDemo {
+
+    @Test(invocationCount = 10)
+    void test() {
+        System.out.println("testing...");
+    }
+
+}
