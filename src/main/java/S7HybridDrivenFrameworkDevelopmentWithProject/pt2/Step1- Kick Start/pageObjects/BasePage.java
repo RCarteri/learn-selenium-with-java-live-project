cd9e56961136be77b934f4fbb341a97aa049e404
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2023-03-14 06:30:21 -0300

    43. Framework Designing - Part 3

--- src/main/java/S7HybridDrivenFrameworkDevelopmentWithProject/pt2/Step1- Kick Start/pageObjects/BasePage.java
+++ src/main/java/S7HybridDrivenFrameworkDevelopmentWithProject/pt2/Step1- Kick Start/pageObjects/BasePage.java
@@ -1,21 +1,21 @@
-//package pageObjects;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.PageFactory;
-//
-//public class BasePage {
-//
-//	 WebDriver driver;
-//
-//	   public BasePage(WebDriver driver)
-//	     {
-//		     this.driver=driver;
-//		     PageFactory.initElements(driver,this);
-//	     }
-//
-//}
-//
-//
-//
-//
-//
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+	 WebDriver driver;
+
+	   public BasePage(WebDriver driver)
+	     {
+		     this.driver=driver;
+		     PageFactory.initElements(driver,this);
+	     }
+
+}
+
+
+
+
+
